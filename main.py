ce70d7a089611216f9423286d3095e00eb48ee29
Brandon <brandonsmith114@gmail.com>
2017-02-10 07:45:09 -0500

    Adds sql views and updates import script

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-'''creates tables and import turnstile data'''
 from sqlalchemy import create_engine, Table, MetaData
 import database as db
 import turnstile
@@ -11,13 +10,16 @@ db.Base.metadata.create_all(engine)
 meta = MetaData(bind=engine)
 readings = Table('readings', meta, autoload=True)
 
-link = turnstile.mta_turnstile_readings()[0]
+conn = engine.connect()
 
-html = r.get(link)
-text = html.text
+columns = ['ca', 'unit', 'scp', 'station', 'linename', 'division', 'date', 'time', 'desc', 'entries', 'exits']
 
-data = csv.reader(StringIO(text))
+links = turnstile.mta_turnstile_readings()[0:5]
 
-data.__next__()
-
-data_list = list(data)
+for link in links:
+    print("Import from: ", link)
+    html = r.get(link)
+    data = csv.reader(StringIO(html.text))
+    data.__next__() #skips headers in first row
+    data = [dict(zip(columns, row)) for row in list(data)]
+    conn.execute(readings.insert(), data)
---
+++ readings.sql
@@ -0,0 +1,36 @@
+﻿CREATE VIEW counts 
+AS
+	SELECT 
+		r.station
+		,r.linename
+		,r."date"
+		,r."time"
+		,r.exits
+		,r.entries
+		,ABS(r.exits - LAG(r.exits, 1, r.exits) OVER (PARTITION BY r.ca || r.unit || r.scp ORDER BY r."date" ASC, r."time" ASC)) as diff_exits
+		,ABS(r.exits - first_value(exits) OVER (PARTITION BY r.ca || r.unit || r.scp ORDER BY r."date" ASC,r."time" ASC)) as cum_exits
+		,ABS(r.entries - LAG(r.entries, 1, r.entries) OVER (PARTITION BY r.ca || r.unit || r.scp ORDER BY r."date" ASC,r."time" ASC)) diff_entries
+		,ABS(r.entries - first_value(entries) OVER (PARTITION BY r.ca || r.unit || r.scp ORDER BY r."date" ASC,r."time" ASC)) as cum_entries
+	FROM readings r
+	WHERE date_part('second', r."time") = 0
+		AND date_part('minute', r."time") = 0;
+	
+
+select 
+	c.station
+	,c.linename
+	,c."date"
+	,c."time"
+	,sum(diff_entries)
+	,sum(cum_entries)
+from counts c
+where 
+	station = 'BEDFORD AV'
+group by
+	c.station
+	,c.linename
+	,c."date"
+	,c."time"
+order by
+	c."date"
+	,c."time"
